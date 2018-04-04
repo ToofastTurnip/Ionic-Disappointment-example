7ed230d9251f80573c57a097ff5778a3d2af07fa
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-04 08:41:19 -0400

    New test added

--- src/providers/magic-ball/magic-ball.spec.ts
+++ src/providers/magic-ball/magic-ball.spec.ts
@@ -9,27 +9,24 @@ describe('Magic 8 Ball Service', () => {
     });
  
     it('should return a non empty array', () => {
- 
-            let result = magicBall.getAnswers();
- 
-            expect(Array.isArray(result)).toBeTruthy;
-            expect(result.length).toBeGreaterThan(0);
-        }
-    );
+        let result = magicBall.getAnswers();
+        expect(Array.isArray(result)).toBeTruthy;
+        expect(result.length).toBeGreaterThan(0);
+    });
  
     it('should return one random answer as a string', () => {
-            expect(typeof magicBall.getRandomAnswer()).toBe('string');
-        }
-    );
+        expect(typeof magicBall.getRandomAnswer()).toBe('string');
+    });
  
     it('should have both yes and no available in result set', () => {
- 
-            let result = magicBall.getAnswers();
- 
-            expect(result).toContain('Yes');
-            expect(result).toContain('No');
- 
-        }
-    );
+        let result = magicBall.getAnswers();
+        expect(result).toContain('Yes');
+        expect(result).toContain('No');
+    });
+
+    it('should be a number', () => {
+        let result = magicBall.getRandomInt(1,3);
+        expect(result).not.toBeNaN();
+    });
  
 });
